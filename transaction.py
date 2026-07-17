33c8fe9410856f98599fe18bf6db9624c484bbc2
Nabila-Ikram <nabeelaqueen99@gmail.com>
2026-07-17 16:40:38 -0700

    CLI

--- transaction.py
+++ transaction.py
@@ -6,7 +6,7 @@ import datetime as  dt
 import json
 
 #json.dump  write json directly into file... write to disk 
-#json.dumps giev the json string no need of  file .. write to memory
+#json.dumps gives the json string no need of  file .. write to memory
 # json.load(f) — reads JSON from a file
 # json.loads(s) — reads JSON from a string you already have in memory (no file involved)
 #Transaction Class
@@ -38,7 +38,11 @@ class Transaction:
 
 #Account Class
 class Account:
-   def __init__(self, owner_name):
+   def __init__(self, owner_name,account_id=None):
+      if(account_id is None):
+         self.account_id=str(uuid.uuid4())
+      else:
+         self.account_id=account_id   
       self.owner_name = owner_name
       self.transactions=[]
       self.balance=0
@@ -70,34 +74,106 @@ class Account:
     else:
        raise ValueError("Id not found")
 
-   def json_conversion (self):
-       data_save=[s.to_dict() for s in self.get_transactions()]
-       return data_save
-   
-   def save_to_file(self,filename):
-           with open (filename,"w") as f:
-             json.dump(self.json_conversion(),f,indent=4,skipkeys=True,ensure_ascii=False)
-    
-   def load_from_file(self,filename):
-      with open (filename,"r") as f:
-         load_data=json.load(f)
-      for v in load_data :
-        self.add_transaction( Transaction(v["amount"],v["category"],v["transaction_type"],v["description"],v["trans_id"],dt.datetime.strptime(v["date"],"%d/%m/%y %I:%M %p")))
+   def to_dict(self):
+      return {
+         " owner_name":self.owner_name,
+          "account_id":self.account_id,
+          "transactions":[s.to_dict() for s in self.get_transactions()]
 
+      }    
+   def save_to_file(self,filename):
+  #takes two argument (data ..obj to save and file //json dump
+    try:
+      with open (filename,"r") as f:
+       load_data=json.load(f)
+      for  (index,d)  in enumerate (load_data):
+        if(d["account_id"]==self.account_id):
+           load_data[index]=self.to_dict()
+           break
+      else:
+           load_data.append(self.to_dict())
+      with open (filename,"w") as f:
+       json.dump(load_data,f,indent=4,skipkeys=True,ensure_ascii=False)            
+           
+    except FileNotFoundError:
+     load_data=[]
+     load_data.append(self.to_dict())
+     with open (filename,"w") as f:
+      json.dump(load_data,f,indent=4,skipkeys=True,ensure_ascii=False)
 
  # json.loads convert json strings into python dictionaries
  # json.dumps convert python dictionaries into json strings
  # python internally/automatically do jsons.loads when data  comes from API   
  # to convert data into json convert into universal data types that json understands {dic,lists,strings,int,float}
+acc_owner=input("Enter Account holder name :")  
+def Search_from_file(filename,acc_owner):
+ try:
+   with open (filename,"r") as f:
+    load_data=json.load(f)
+    print(load_data)
+    for d in load_data:
+        if(d["owner_name"]== acc_owner):
+           newacc=Account(d["owner_name"],d["account_id"])
+           for t in d["transactions"] :
+            newacc.add_transaction(Transaction(t["amount"],
+            t["category"],t["transaction_type"],
+            t["description"],t["trans_id"],
+            dt.datetime.strptime(t["date"],"%d/%m/%y %I:%M %p")))
+            return newacc
+        
+    else:   
+     newacc=Account(acc_owner)
+     return newacc
+ except FileNotFoundError:
+     newacc=Account(acc_owner)
+     return newacc
+  
+
+acc=Search_from_file("transactions.json" ,acc_owner)
+choices = [1,2,3,4,5]   
+       
+while True:
+    try:
+        print("""
+1. Add Transaction
+2. View All Transactions
+3. Delete Transaction
+4. View Balance
+5. Exit
+""")
+        user_choice = int(input("Enter your choice: "))
+
+        if user_choice not in choices:
+            raise ValueError("Not a valid choice")
+
+        if user_choice == 5:
+            acc.save_to_file("transactions.json")
+            break
       
-      
-obj1 = Transaction(500,"food","expense")
-print(type(obj1.to_dict()))
-acc=Account("nabila")
-acc.add_transaction(obj1)
-data=acc.json_conversion()
-acc.save_to_file("transactions.json")
-acc.load_from_file("transactions.json")
+        print("You chose", user_choice)
+        if(user_choice==1):
+           amount=int(input("Enter amount : "))
+           category=input("Enter Category : ")
+           transaction_type=input("Enter transaction-type : ")
+           description=input("Enter description : ") 
+           acc.add_transaction(Transaction(amount,category,transaction_type,description))
+        elif(user_choice==2):
+          for t in acc.get_transactions():
+             print(t) #triggers str function for better formatting
+              
+        elif(user_choice==3):    
+           for t in acc.get_transactions():
+             print(t)
+           Trans_id=input("Enter  Enter Transaction_id  you want to delete :")
+           try:
+              acc.remove_transaction(Trans_id)
+           except ValueError:
+              print("Not a valid transaction_id")       
+        elif(user_choice==4):
+         print(f"Balance is :   {acc.get_balance()}")
+    except ValueError as e:
+        print(e)
+
 
 
 
