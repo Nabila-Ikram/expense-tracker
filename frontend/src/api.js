66b8eaa60daf8fd4ace3df9b0d24caaacf3e08d7
Nabila-Ikram <nabeelaqueen99@gmail.com>
2026-08-16 02:09:20 -0700

    Add API configuration

---
+++ frontend/src/api.js
@@ -0,0 +1,3 @@
+// export const API_URL = "https://troubling-lazily-goatskin.ngrok-free.dev";
+// export const API_URL = "http://localhost:5000";
+export const API_URL = "https://NabilaIkram.pythonanywhere.com";
